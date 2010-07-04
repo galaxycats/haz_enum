2f496dc389ba9c5490458d8d409254e02a435679
thyphoon <andi@galaxycats.com>
2010-07-04 13:38:11 +0200

    added method available_field_types_for_sets

--- lib/haz_enum.rb
+++ lib/haz_enum.rb
@@ -2,6 +2,11 @@ require "haz_enum/enum"
 require "haz_enum/set"
 
 module HazEnum
+  class <<self
+    def available_field_types_for_sets
+      [:yml, :bitfield]
+    end
+  end
 end
 
 ActiveRecord::Base.extend HazEnum::Enum
